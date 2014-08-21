12b9563c5141fe96174fe4071bf15ae326500649
Mate J Kovacs <mkovaxx@gmail.com>
2014-08-20 17:58:30 -0700

    half-baked Bezier -> Conic conversion and plotting

--- src/main/Conics.hs
+++ src/main/Conics.hs
@@ -1,4 +1,46 @@
+{-# LANGUAGE RecordWildCards #-}
 module Conics
   (
   ) where
 
+data Bezier =
+  Bezier
+  { bezierP0 :: Vector
+  , bezierP1 :: Vector
+  , bezierP2 :: Vector
+  , bezierW  :: Float
+  }
+
+data Conic =
+  Conic
+  { conicAd :: Vector
+  , conicAs :: Float
+  , conicN  :: Vector
+  , conicD  :: Float
+  }
+
+bezierToConic :: Bezier -> Conic
+bezierToConic Bezier{..} =
+  Conic
+  { conicAd = a_d
+  , conicAs = a_s
+  , conicN  = n
+  , conicD  = d
+  }
+ where
+  p0 = bezierP0 - bezierP1
+  p2 = bezierP2 - bezierP1
+  dif = p0 - p2
+  det = det p0 p2
+  a_d = undefined
+  a_s = undefined
+  n = mulSV (2 * det) dif
+  d = det ^ 2
+
+evalConic :: Conic -> Vector -> Float
+evalConic Conic{..} p =
+  conicD + dotV conicN p
+
+normH :: Vector -> Float -> Vector -> Float
+normH (a, c) b (x, y) =
+  (a * x ^ 2) + (2 * b * x * y) + (c * y ^ 2)
--- src/main/Main.hs
+++ src/main/Main.hs
@@ -2,6 +2,9 @@
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Raster.Field
+
+import Conics
 
 knobRadius = 6
 
@@ -10,66 +13,77 @@ main = do
   let
     position = (10, 10)
   play
-    (InWindow "Nice Window" (size initState) position)
+    (InWindow "Conics" (size initialize) position)
     white
     0
-    initState
-    drawState
-    moveState
-    stepState
+    initialize
+    draw
+    input
+    step
 
 data State =
   State
   { size :: (Int, Int)
   , cursor :: (Float, Float)
-  , point_00 :: (Float, Float)
-  , point_11 :: (Float, Float)
-  , point_01 :: (Float, Float)
+  , point0 :: (Float, Float)
+  , point1 :: (Float, Float)
+  , point2 :: (Float, Float)
   , weight :: Float
   , selection :: Maybe Int
   }
 
-initState :: State
-initState =
+initialize :: State
+initialize =
   State
   { size = (800, 600)
   , cursor = (0, 0)
-  , point_00 = (-40, -40)
-  , point_11 = (250, 250)
-  , point_01 = (200, -200)
+  , point0 = (40, 250)
+  , point1 = (0, 0)
+  , point2 = (200, 60)
   , weight = 1
   , selection = Nothing
   }
 
-drawState :: State -> Picture
-drawState State{..} =
+draw :: State -> Picture
+draw State{..} =
   Color black $ Pictures
-    [ Line [point_00, point_01]
-    , Line [point_01, point_11]
-    , uncurry Translate point_00 $ Circle knobRadius
-    , uncurry Translate point_11 $ Circle knobRadius
-    , uncurry Translate point_01 $ Circle knobRadius
+    [ Line [point0, point1]
+    , Line [point1, point2]
+    , uncurry Translate point0 $ Circle knobRadius
+    , uncurry Translate point1 $ Circle knobRadius
+    , uncurry Translate point2 $ Circle knobRadius
+    , plot
     ]
+ where
+  bezier =
+    Bezier
+    { bezierP0 = point0
+    , bezierP1 = point1
+    , bezierP2 = point2
+    , bezierW  = weight
+    }
+  conic = bezierToConic bezier
+  plot = makePicture (fst size) (snd size) 1 1 ((\v -> if v < 0 then white else purple) . evalConic conic)
 
-moveState :: Event -> State -> State
-moveState event state@State{..} = case event of
+input :: Event -> State -> State
+input event state@State{..} = case event of
   EventResize newSize ->
     state{ size = newSize }
   EventKey (MouseButton LeftButton) Down _ pos ->
-    (if magV (pos - point_00) < knobRadius then state{ selection = Just 0 }
-    else if magV (pos - point_11) < knobRadius then state{ selection = Just 1 }
-    else if magV (pos - point_01) < knobRadius then state{ selection = Just 2 }
+    (if magV (pos - point0) < knobRadius then state{ selection = Just 0 }
+    else if magV (pos - point2) < knobRadius then state{ selection = Just 2 }
+    else if magV (pos - point1) < knobRadius then state{ selection = Just 1 }
     else state){ cursor = pos }
   EventKey (MouseButton LeftButton) Up _ pos ->
     state{ selection = Nothing, cursor = pos }
   EventMotion pos ->
     (case selection of
       Nothing -> state
-      Just 0  -> state{ point_00 = point_00 + pos - cursor }
-      Just 1  -> state{ point_11 = point_11 + pos - cursor }
-      Just 2  -> state{ point_01 = point_01 + pos - cursor }
+      Just 0  -> state{ point0 = point0 + pos - cursor }
+      Just 1  -> state{ point1 = point1 + pos - cursor }
+      Just 2  -> state{ point2 = point2 + pos - cursor }
       ){cursor = pos}
   _ -> state
 
-stepState :: Float -> State -> State
-stepState _ = id
+step :: Float -> State -> State
+step _ = id
